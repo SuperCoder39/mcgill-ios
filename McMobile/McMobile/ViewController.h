4cd4fbfe5786f9735a3abda826cec54af6dc9ec6
Dan McGill <dmcgill@jjkeller.com>
2017-12-01 14:59:13 -0500

    Initial Commit

---
+++ McMobile/McMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  McMobile
+//
+//  Created by McGill, Daniel (Dan) J on 12/1/17.
+//  Copyright © 2017 McGill, Daniel (Dan) J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ McMobile/McMobile/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  McMobile
+//
+//  Created by McGill, Daniel (Dan) J on 12/1/17.
+//  Copyright © 2017 McGill, Daniel (Dan) J. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
